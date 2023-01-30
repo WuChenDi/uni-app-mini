11a110a0608c8e57ef3bc68df1d33b09cb1a6d46
WuChenDi <wuchendi96@gmail.com>
2023-01-30 16:43:51 +0800

    feat: modify home page style namespace and home page title

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,17 +7,13 @@
       <text class="hello-text"> {{ title }} </text>
     </view>
 
-    <view class="demo-home-nav">
-      <!-- <view class="demo-home-nav__title">基础组件</view> -->
-      <view class="demo-home-nav__group">
-        <view
-          v-for="(item, index) in baseListRef"
-          class="demo-home-nav__block"
-          @click="goDetailPage(item.pathName)"
-          :key="index"
-        >
+    <view class="uni-home-nav">
+      <!-- <view class="uni-home-nav__title">基础组件</view> -->
+      <view class="uni-home-nav__group">
+        <view v-for="(item, index) in baseListRef" class="uni-home-nav__block" @click="goDetailPage(item.pathName)"
+          :key="index">
           {{ item.name }}
-          <image class="common-arrow-right" src="https://cos.wshoto.com/marketing-wxapp/group-punch/a18.png" />
+          <image class="uni-common-arrow-right" src="https://cos.wshoto.com/marketing-wxapp/group-punch/a18.png" />
         </view>
       </view>
     </view>
@@ -50,180 +46,69 @@ const goDetailPage = (name: string) => {
 }
 </script>
 
-<style scoped>
-/* #ifndef APP-PLUS-NVUE */
+<style scoped lang="scss">
 page {
   min-height: 100%;
   height: auto;
 }
 
-/* #endif */
-
-/* 解决头条小程序字体图标不显示问题，因为头条运行时自动插入了span标签，且有全局字体 */
-/* #ifdef MP-TOUTIAO */
-/* text :not(view) {
-    font-family: uniicons;
-} */
-/* #endif */
-
-.uni-icon {
-  font-family: uniicons;
-  font-weight: normal;
-}
-
 .uni-container {
   padding: 15px 40rpx;
   background: #fff;
-}
 
-.uni-header-logo {
-  display: flex;
-  padding: 15px 15px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10rpx;
-}
+  .uni-header-logo {
+    display: flex;
+    padding: 15px 15px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10rpx;
 
-.uni-header-image {
-  width: 80px;
-  height: 80px;
-  border-radius: 10px;
-}
+    .uni-header-image {
+      width: 80px;
+      height: 80px;
+      border-radius: 10px;
+    }
+  }
 
-.uni-hello-text {
-  margin-bottom: 20px;
-}
+  .uni-hello-text {
+    margin-bottom: 20px;
 
-.hello-text {
-  color: #7a7e83;
-  font-size: 14px;
-  line-height: 20px;
-}
+    .hello-text {
+      color: #7a7e83;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
 
-.hello-link {
-  color: #7a7e83;
-  font-size: 14px;
-  line-height: 20px;
-}
+  .uni-home-nav {
+    padding-bottom: 100px;
+  }
 
-.uni-panel {
-  margin-bottom: 12px;
-}
+  .uni-home-nav__title {
+    margin: 0 0 10px 10px;
+    color: rgba(69, 90, 100, 0.6);
+    font-size: 14px;
+  }
 
-.uni-panel-h {
-  /* #ifdef H5 */
-  display: flex;
-  /* #endif */
-  background-color: #ffffff;
-  flex-direction: row !important;
-  /* justify-content: space-between !important; */
-  align-items: center !important;
-  padding: 12px;
-  /* #ifdef H5 */
-  cursor: pointer;
-  /* #endif */
-}
+  .uni-home-nav__block {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 0 12px;
+    padding: 0 15px;
+    color: #323233;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 40px;
+    background: #f7f8fa;
+    border-radius: 99px;
+    transition: background 0.3s;
+  }
 
-/*
-.uni-panel-h:active {
-    background-color: #f8f8f8;
-}
- */
-.uni-panel-h-on {
-  background-color: #f0f0f0;
-}
-
-.uni-panel-text {
-  flex: 1;
-  color: #000000;
-  font-size: 14px;
-  font-weight: normal;
-}
-
-.uni-panel-icon {
-  margin-left: 15px;
-  color: #999999;
-  font-size: 14px;
-  font-weight: normal;
-  transform: rotate(0deg);
-  transition-duration: 0s;
-  transition-property: transform;
-}
-
-.uni-panel-icon-on {
-  transform: rotate(180deg);
-}
-
-.uni-navigate-item {
-  /* #ifdef H5 */
-  display: flex;
-  /* #endif */
-  flex-direction: row;
-  align-items: center;
-  background-color: #ffffff;
-  border-top-style: solid;
-  border-top-color: #f0f0f0;
-  border-top-width: 1px;
-  padding: 12px;
-  /* #ifdef H5 */
-  cursor: pointer;
-  /* #endif */
-}
-
-.uni-navigate-item:active {
-  background-color: #f8f8f8;
-}
-
-.uni-navigate-text {
-  flex: 1;
-  color: #000000;
-  font-size: 14px;
-  font-weight: normal;
-}
-
-.uni-navigate-icon {
-  margin-left: 15px;
-  color: #999999;
-  font-size: 14px;
-  font-weight: normal;
-}
-
-.demo-home__title {
-  padding: 40px 0 100px 0;
-  margin: 0;
-}
-
-.demo-home-nav {
-  padding-bottom: 100px;
-}
-
-.demo-home-nav__title {
-  margin: 0 0 10px 10px;
-  color: rgba(69, 90, 100, 0.6);
-  font-size: 14px;
-  /* // .fontsize(14px); */
-}
-
-.demo-home-nav__block {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 0 0 24rpx;
-  padding-left: 20rpx;
-  color: #323233;
-  font-weight: 600;
-  font-size: 28rpx;
-  /* // .fontsize(14px); */
-  line-height: 80rpx;
-  background: #f7f8fa;
-  border-radius: 99rpx;
-  transition: background 0.3s;
-}
-
-.common-arrow-right {
-  width: 32rpx;
-  height: 32rpx;
-  margin-right: 16rpx;
+  .uni-common-arrow-right {
+    width: 20px;
+    height: 20px;
+  }
 }
 </style>
