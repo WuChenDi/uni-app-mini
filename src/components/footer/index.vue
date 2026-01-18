dc7301241f69164146dc51750530ad3a18de2f6e
wudi <wuchendi96@gmail.com>
2026-01-18 17:51:23 +0800

    feat(home): migrate to UnoCSS and improve layout
    - Replace traditional CSS with UnoCSS utility classes for better
      maintainability
    - Update component structure to use modern utility-first styling
    - Remove custom SCSS styles in favor of UnoCSS atomic approach
    - Update avatar image from PNG to JPG format
    - Add proper component name definition using defineOptions

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <view class="footer_container">
-    <uni-link href="https://github.com/WuChenDi" text="wudi"></uni-link>
+  <view class="fixed left-0 bottom-60rpx w-full h-37rpx lh-37rpx text-center text-26rpx font-bold text-[#909399]">
+    <uni-link
+      href="https://github.com/WuChenDi"
+      text="wudi"
+      class="px-20rpx"
+    />
   </view>
 </template>
 
@@ -9,23 +13,3 @@ defineOptions({
   name: 'Footer',
 })
 </script>
-
-<style scoped lang="scss">
-.footer_container {
-  position: fixed;
-  left: 0;
-  bottom: 60rpx;
-  width: 100%;
-  height: 37rpx;
-  line-height: 37rpx;
-  text-align: center;
-  font-size: 26rpx;
-  font-family: PingFang-SC-Bold, PingFang-SC;
-  font-weight: bold;
-  color: rgba(144, 147, 153, 1);
-}
-
-.footer_container text {
-  padding: 20rpx;
-}
-</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <view class="uni-container">
-    <view class="uni-header-logo">
-      <image class="uni-header-image" src="/static/avatar.png"></image>
-    </view>
-    <view class="uni-hello-text">
-      <text class="hello-text"> {{ title }} </text>
+  <view class="p-[15px_40rpx] bg-white min-h-screen">
+    <view class="center flex-col py-15px mt-10rpx">
+      <image
+        class="w-80px h-80px rd-10px"
+        src="/static/avatar.jpg"
+      />
     </view>
 
-    <view class="uni-home-nav">
-      <!-- <view class="uni-home-nav__title">基础组件</view> -->
-      <view class="uni-home-nav__group">
-        <view v-for="(item, index) in baseListRef" class="uni-home-nav__block" @click="goDetailPage(item.pathName)"
-          :key="index">
-          {{ item.name }}
-          <image class="uni-common-arrow-right" src="https://cos.wshoto.com/marketing-wxapp/group-punch/a18.png" />
+    <view class="mb-20px">
+      <text class="text-[#7a7e83] text-14px lh-20px">
+        {{ title }}
+      </text>
+    </view>
+
+    <view class="pb-100px pb-safe">
+      <view class="m-[0_0_10px_10px] text-[rgba(69,90,100,0.6)] text-14px">
+        基础组件
+      </view>
+
+      <view class="flex flex-col gap-12px">
+        <view
+          v-for="(item, index) in baseListRef"
+          :key="index"
+          class="flex items-center justify-between px-15px text-[#323233] font-600 text-14px lh-40px bg-[#f7f8fa] rd-99px active:opacity-70 transition-all"
+          @click="goDetailPage(item.pathName)"
+        >
+          <text>{{ item.name }}</text>
+          <image
+            class="w-20px h-20px"
+            src="https://cos.wshoto.com/marketing-wxapp/group-punch/a18.png"
+          />
         </view>
       </view>
     </view>
@@ -24,6 +40,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Footer from '@/components/footer/index.vue'
+
+defineOptions({
+  name: 'Home',
+})
 
 const title = ref('Hello, 业务开发记录一些组件')
 const baseListRef = ref([
@@ -48,65 +68,5 @@ const goDetailPage = (name: string) => {
 page {
   min-height: 100%;
   height: auto;
-}
-
-.uni-container {
-  padding: 15px 40rpx;
-  background: #fff;
-
-  .uni-header-logo {
-    display: flex;
-    padding: 15px 15px;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10rpx;
-
-    .uni-header-image {
-      width: 80px;
-      height: 80px;
-      border-radius: 10px;
-    }
-  }
-
-  .uni-hello-text {
-    margin-bottom: 20px;
-
-    .hello-text {
-      color: #7a7e83;
-      font-size: 14px;
-      line-height: 20px;
-    }
-  }
-
-  .uni-home-nav {
-    padding-bottom: 100px;
-  }
-
-  .uni-home-nav__title {
-    margin: 0 0 10px 10px;
-    color: rgba(69, 90, 100, 0.6);
-    font-size: 14px;
-  }
-
-  .uni-home-nav__block {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 0 0 12px;
-    padding: 0 15px;
-    color: #323233;
-    font-weight: 600;
-    font-size: 14px;
-    line-height: 40px;
-    background: #f7f8fa;
-    border-radius: 99px;
-    transition: background 0.3s;
-  }
-
-  .uni-common-arrow-right {
-    width: 20px;
-    height: 20px;
-  }
 }
 </style>
